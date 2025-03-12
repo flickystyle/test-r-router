bf1cbaf51f6d777e34d0b9f34264a1f188360930
Dmitry <superflick3@gmail.com>
2025-03-12 16:18:43 +0300

    Testing fake authentication

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,12 +1,17 @@
+import { useAuth } from '../hooks/useAuth';
 import Map from '../components/Map';
 import Sidebar from '../components/Sidebar';
 import styles from './AppLayout.module.css';
+import User from '../components/User';
 
 function AppLayout() {
+    const { user } = useAuth();
+
     return (
         <div className={styles.app}>
             <Sidebar />
             <Map />
+            {user && <User />}
         </div>
     );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useAuth } from '../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 import PageNav from '../components/PageNav';
 import styles from './Login.module.css';
+import Button from '../components/Button';
 
 export default function Login() {
-    // PRE-FILL FOR DEV PURPOSES
-    const [email, setEmail] = useState('jack@example.com');
+    const [email, setEmail] = useState('vasiliy@example.com');
     const [password, setPassword] = useState('qwerty');
+    const { login, isAuthenticated } = useAuth();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate('/app', { replace: true });
+        }
+    }, [isAuthenticated, navigate]);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        login(email, password);
+    }
 
     return (
         <main className={styles.login}>
             <PageNav />
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <div className={styles.row}>
                     <label htmlFor="email">Email address</label>
                     <input
@@ -31,8 +47,8 @@ export default function Login() {
                     />
                 </div>
 
-                <div>
-                    <button>Login</button>
+                <div className={styles.btn}>
+                    <Button type="primary">LOGIN</Button>
                 </div>
             </form>
         </main>
